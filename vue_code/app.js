160773c79b58fd9e3f4c131552e9f1807715a61a
nuruzzamanhimel00 <nuruzzaman.himel147@gmail.com>
2022-08-02 10:41:34 +0600

    bind

---
+++ vue_code/app.js
@@ -0,0 +1,10 @@
+const app = Vue.createApp({
+    data(){
+        return {
+            title: "hello himel",
+            linkGoTo: "https://vuejs.org/guide/essentials/application.html#mounting-the-app"
+        }
+    }
+});
+app.mount('#app');
+  
