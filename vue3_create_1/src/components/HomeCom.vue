b2b64b32487dbbc7ded3561cb1eb1cf1d66841cb
nuruzzamanhimel00 <nuruzzaman.himel147@gmail.com>
2022-08-10 12:26:36 +0600

    tut 50 com

--- vue3_create_1/src/components/HomeCom.vue
+++ vue3_create_1/src/components/HomeCom.vue
@@ -1,5 +1,17 @@
 <template>
     <div>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus veniam voluptate ducimus. Sapiente dolor quia, iste voluptatem blanditiis iusto laborum minus earum libero excepturi cum mollitia omnis aliquid voluptates commodi.
+        <a href="" class="btn btn-success" @click.prevent="goToThePost()">
+            Go to the Post
+        </a>
     </div>
 </template>
+<script>
+export default {
+    methods:{
+        goToThePost(){
+            this.$router.push({name: 'userDetails',query:{id:1} });
+        }
+    }
+}
+</script>
