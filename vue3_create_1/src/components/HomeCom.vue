8c72025d77a2446e2be093a43d9ac44d550777b4
nuruzzamanhimel00 <nuruzzaman.himel147@gmail.com>
2022-08-14 11:26:16 +0600

    map action, mapmutiaon, map

--- vue3_create_1/src/components/HomeCom.vue
+++ vue3_create_1/src/components/HomeCom.vue
@@ -2,6 +2,8 @@
     <div>
     <h1>{{ getCcountResult }}</h1>
     <a href="" class="btn btn-warning btn-sm" v-on:click.prevent="onCountIncrement()" >Count Increment</a>
+    &nbsp;
+        <a href="" class="btn btn-warning btn-sm" v-on:click.prevent="onCountIncrementActionA()" >Count Action A Incsrement</a>
     <hr>
     <p>To do list done cout: ( {{ doneTodolistCount }} )</p>
     <hr>
@@ -11,7 +13,7 @@
     </div>
 </template>
 <script>
-import {mapGetters} from 'vuex'
+import {mapGetters, mapActions} from 'vuex'
 
 import { defineAsyncComponent } from 'vue'
 
@@ -25,6 +27,9 @@ const TodolistComp = defineAsyncComponent(() =>
 
 export default {
     methods:{
+        ...mapActions({
+            incrementValue:'increment'
+        }),
         goToThePost(){
             this.$router.push({name: 'userDetails',query:{id:1} });
         },
@@ -33,7 +38,19 @@ export default {
             // this.$store.commit('increment',5);
             // this.$store.commit('increment',{value:5});
 
-            this.$store.dispatch('increment',{value:5});
+            // this.$store.dispatch('increment',{value:5});
+            
+            this.incrementValue({value:5});
+        },
+        onCountIncrementActionA(){
+            // this.$store.dispatch('actionA').then( (response) => {
+            //     console.log("response console");
+            //     console.log(response);
+            // } );
+            this.$store.dispatch('actionB').then( (response) => {
+                console.log("response console");
+                console.log(response);
+            } );
         }
     },
     computed:{
--- vue3_create_1/src/store/index.js
+++ vue3_create_1/src/store/index.js
@@ -47,9 +47,26 @@ const store = createStore({
     },
     actions:{
       async increment(context, payload){
-        console.log(context);
+        // console.log(context);
         context.commit('increment', payload);
+      },
+      actionA(context){
+        // return new Promise((resolve, reject) => {
+        return new Promise((resolve) => {
+          setTimeout(() => {
+            context.commit('increment', {value:3});
+            resolve("Promise success response return");
+          }, 2000);
+        });
+      },
+      actionB(context){
+        // 78.78. Dispatching Actions within another actions in Vuex - Vue js _ Vue 3.
+        context.dispatch('actionA').then( (response) =>{
+          console.log(response);
+          console.log('action B calling actionA');
+        } );
       }
+
     }
   });
 
