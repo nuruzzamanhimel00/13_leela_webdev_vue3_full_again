14ea9abbe98ca613b3da2dd713a7fd894d6d0d87
nuruzzamanhimel00 <nuruzzaman.himel147@gmail.com>
2022-09-13 00:58:53 +0600

    loader set

--- vue-authorization/src/store/modules/post/index.js
+++ vue-authorization/src/store/modules/post/index.js
@@ -1,4 +1,4 @@
-import { FETCH_ALL_POST, GET_ALL_POST_ACTION, SET_ALL_POST } from "@/store/storeconstants";
+import { FETCH_ALL_POST, GET_ALL_POST_ACTION, LOADING_SPINING_SHOW_MUTATION, SET_ALL_POST } from "@/store/storeconstants";
 import axios from 'axios'
 import {ApiRoute} from '../../../Helpers/ApiRoute.js'
 import Api from '../../../Helpers/Api.js'
@@ -21,19 +21,24 @@ export default {
     },
     actions:{
         [GET_ALL_POST_ACTION](context){
+          context.commit(LOADING_SPINING_SHOW_MUTATION,true,{root:true} );
+             // if , zdi eta onno kono module hto tahle jevabe likte hto ta hlo
+        // context.commit(moduleName/LOADING_SPINING_SHOW_MUTATION,true);
           axios.get(ApiRoute.get_all_post,{
             headers: Api.getHeaderWitAuth()
           })
           .then( (response) => {
-            console.log(response);
+            // console.log(response);
             if(response.status == 200){
-
+              context.commit(LOADING_SPINING_SHOW_MUTATION,false,{root:true} );
               context.commit(SET_ALL_POST, response.data.data)
-              // context.commit(LOADING_SPINING_SHOW_MUTATION,false,{root:true});
+              // context.commit(LOADING_SPINING_SHOW_MUTATION,false,{root:false});
+                 // if , zdi eta onno kono module hto tahle jevabe likte hto ta hlo
+              // context.commit(moduleName/LOADING_SPINING_SHOW_MUTATION,true);
             }
           })
           .catch(function () {
-           
+            context.commit(LOADING_SPINING_SHOW_MUTATION,false,{root:true} );
           });
         }
     },
