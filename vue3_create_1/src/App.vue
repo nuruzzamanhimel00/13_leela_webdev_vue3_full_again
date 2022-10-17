9ecf90a13d3fae4d6a55bde0a5c5ef4e51efcfa2
nuruzzamanhimel00 <nuruzzaman.himel147@gmail.com>
2022-10-17 22:29:07 +0600

    watcher

--- vue3_create_1/src/App.vue
+++ vue3_create_1/src/App.vue
@@ -2,10 +2,18 @@
   <div>
     <div class="container">
     <div class="row">
-      <div class="col-md-4">
+      <div class="col-md-6">
         <user-create @getAllUserDetailsFlug="ongetAllUserDetailsFlug($event)"> </user-create>
+        <br>
+        <hr>
+        <h1>Watcher Practice...</h1> <br>
+        <input type="text" v-model="movie">
+        <input type="text" v-model="movieInfo.title">
+        <input type="text" v-model="movieInfo.actor">
+        <button @click.prevent="onAddMovieList">Add Movie List</button>
+
       </div>
-      <div class="col-md-4">
+      <div class="col-md-6">
         <user-detail :user-created-flug="getAllUserDetails"  
          @getAllUserDetailsFlug="ongetAllUserDetailsFlug($event)"
         > </user-detail>
@@ -13,6 +21,7 @@
       </div>
       <div class="col-md-4"></div>
     </div>
+
       
     </div>
   </div>
@@ -33,6 +42,12 @@ export default {
   data() {
     return {
       getAllUserDetails:false,
+      movie:'Spiderman',
+      movieInfo:{
+        title:'',
+        actor:'',
+      },
+      movieList:['superman','batman','antman'],
     }
   },
   components:{
@@ -41,9 +56,33 @@ export default {
     UserDetail
 
   },
+  watch:{
+    movie: {
+      handler(newValue) {
+         console.log(`Movie name is ${newValue}`);
+      },
+      // force eager callback execution
+      immediate: true
+    },
+    movieInfo: {
+      handler(newValue) {
+         console.log(`movieInfo chnaage title is ${newValue.title} and actor is ${newValue.actor}` );
+      },
+      deep: true
+    },
+    movieList: {
+      handler(newValue) {
+         console.log(`movieInfo chnaage  is ${newValue}` );
+      },
+      deep: true
+    },
+  },
   methods: { 
     ongetAllUserDetailsFlug(event){
       this.getAllUserDetails = event;
+    },
+    onAddMovieList(){
+      this.movieList.push('new man');
     }
   },
 }
