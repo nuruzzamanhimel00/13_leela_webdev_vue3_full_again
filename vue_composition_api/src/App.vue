b57e315969e505626cf2b098881e3242a2c93b6a
nuruzzamanhimel00 <nuruzzaman.himel147@gmail.com>
2022-12-01 00:27:18 +0600

    done

--- vue_composition_api/src/App.vue
+++ vue_composition_api/src/App.vue
@@ -1,25 +1,42 @@
 <template>
   <div>
       Channel name is : {{ channelName }}
+      <h1>User Details:</h1>
+      <p>name: {{userDetais.name}}</p>
+      <p>age: {{userDetais.age}}</p>
+   
   </div>
 </template>
 
 <script>
 
-import {ref} from 'vue'
+import {ref, reactive} from 'vue'
 
 export default {
 
     setup(){
         let name = ref("Md Nuruzzaman Himel");
 
+        // let userDetais = ref({
+        //     name: 'Himel',
+        //     age: 30
+        // });
+        let userDetais = reactive({
+            name: 'Himel',
+            age: 30
+        });
+
         setTimeout(()=>{
             console.log('settime out in setup console llog');
-            name.value= 'himel'
+            name.value= 'himel';
+            userDetais.name = "modified Nuruzzaman himel";
+            userDetais.age = 100;
+
         },3000);
-        
+    
         return {
-            channelName: name
+            channelName: name,
+            userDetais: userDetais
         }
     }
 
