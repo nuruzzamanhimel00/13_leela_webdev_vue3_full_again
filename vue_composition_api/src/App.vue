a4a59be4460aabdf2a432ebf9703257f045cefe8
nuruzzamanhimel00 <nuruzzaman.himel147@gmail.com>
2022-11-30 23:55:13 +0600

    composition api

---
+++ vue_composition_api/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+      Channel name is : {{ channelName }}
+  </div>
+</template>
+
+<script>
+
+import {ref} from 'vue'
+
+export default {
+
+    setup(){
+        let name = ref("Md Nuruzzaman Himel");
+
+        setTimeout(()=>{
+            console.log('settime out in setup console llog');
+            name.value= 'himel'
+        },3000);
+        
+        return {
+            channelName: name
+        }
+    }
+
+
+//   name: 'App',
+//   data(){
+//     return{
+//       channelName : 'Nuruzzaman Himel'
+//     }
+//   },
+//   mounted(){
+//     setTimeout(()=>{
+//       this.channelName = 'Himel'
+//     },3000)
+//   },
+//   components: {
+    
+//   }
+}
+</script>
+
+<style>
+
+</style>
