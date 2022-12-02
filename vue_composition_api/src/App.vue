f3f7983e6d85831f3231dc4ff1698b7d767fb0a5
nuruzzamanhimel00 <nuruzzaman.himel147@gmail.com>
2022-12-02 11:26:17 +0600

    computed property

--- vue_composition_api/src/App.vue
+++ vue_composition_api/src/App.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
+        <h2>FUll name: {{ fullName }} </h2>
+        <input type="text" @input="changeFirstName($event)"> &nbsp;
+        <input type="text" @input="changeLastName($event)"> ;
+        <br>
       Channel name is : {{ channelName }}
       <h1>User Details:</h1>
       <p>name: {{userDetais.name}}</p>
@@ -12,13 +16,15 @@
 
 <script>
 
-import {ref, reactive, isRef, isReactive } from 'vue'
+import {ref, reactive, isRef, isReactive, computed } from 'vue'
 
 
 export default {
 
     setup(){
         let name = ref("Md Nuruzzaman Himel");
+        let firstName = ref('');
+        let lastName = ref('');
 
         // let userDetais = ref({
         //     name: 'Himel',
@@ -28,6 +34,20 @@ export default {
             name: 'Himel',
             age: 30
         });
+        
+        const fullName = computed(()=>{
+            return firstName.value+ ' '+lastName.value;
+        });
+
+
+        function changeFirstName(event){
+            // console.log(firstName, event)
+            firstName.value = event.target.value;
+        }
+        function changeLastName(event){
+            // console.log(lastName, event);
+            lastName.value = event.target.value;
+        }
 
         console.log(isRef(name));
         console.log(isReactive(userDetais));
@@ -40,7 +60,10 @@ export default {
         return {
             channelName: name,
             userDetais,
-            onChangeName
+            onChangeName,
+            changeFirstName,
+            changeLastName,
+            fullName
         }
     }
 
